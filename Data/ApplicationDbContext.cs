6784932ce447a7b6a38f7275147d991cfd3cdd94
Elek, David <dave.elek.96@gmail.com>
2023-09-05 22:55:36 +0200

    Add SQLite connection string and use it
    Additional formattings in Program.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,8 +14,9 @@ public class ApplicationDbContext : IdentityDbContext<User>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.Entity<User>().ToTable("Users");
     }
+
+    // public DbSet<Xyz> Case { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using motoWhere.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("SqliteDatabase") ?? throw new InvalidOperationException("Connection string 'SqliteDatabase' not found.");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(connectionString)
+);
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<User>(
+    options => options.SignIn.RequireConfirmedAccount = true
+).AddRoles<IdentityRole>(
+).AddEntityFrameworkStores<ApplicationDbContext>(
+);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -20,6 +26,17 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireUppercase = true;
     options.Password.RequiredLength = 6;
     options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    // options.Cookie.HttpOnly = true;
+    // options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+
+    options.LoginPath = "/Identity/Account/Login";
+    // options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    // options.SlidingExpiration = true;
 });
 
 var app = builder.Build();
